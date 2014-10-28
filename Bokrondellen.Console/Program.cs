e16405eb5c985427db32a423cee8a45335f68508
chrhei <christer.heinback@consid.se>
2014-10-28 15:41:32 +0100

    Added EPiServer.Commerce from nuget

--- Bokrondellen.Console/Program.cs
+++ Bokrondellen.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bokrondellen.Initialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,6 +19,7 @@ namespace Bokrondellen.Console
         private static void Initialize(string[] args)
         {
             ParseArgs(args);
+            InitializeFramework.Initialize();
         }
 
         static void ParseArgs(string[] args)
