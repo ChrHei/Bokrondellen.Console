a7a1324270aa872ed39f86ed4a181c81f78a5ef5
chrhei <christer.heinback@consid.se>
2014-10-28 13:51:29 +0100

    Added initialization project

---
+++ Bokrondellen.Initialization/NullTypeScanner.cs
@@ -0,0 +1,14 @@
+﻿using EPiServer.Framework.TypeScanner;
+using System;
+using System.Collections.Generic;
+
+namespace Bokrondellen.Initialization
+{
+	class NullTypeScanner : ITypeScannerLookup
+	{
+		public IEnumerable<Type> AllTypes
+		{
+			get { return new Type[0]; }
+		}
+	}
+}
